7a8e7873c8611ce2865d890ad319bda3a943bbc6
S74RK1113R <randyes16@gmail.com>
2025-09-13 03:23:14 -0400

    blog-preview-card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BlogCard from "./components/BlogCard";
+import "@fontsource-variable/figtree";
+
+function App() {
+ 
+  return (
+    <main className="h-full flex flex-col justify-center items-center">
+      <BlogCard/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,31 @@
+import avatar from "../assets/image-avatar.webp";
+import ilustratioArticle from "../assets/illustration-article.svg";
+
+export default function BlogCard() {
+  return (
+    
+    <div className="w-sm bg-white rounded-3xl flex flex-col items-start justify-center border-2 shadow-blog-Gray-950 shadow-blog-shadow-Gray-950">
+        <div className="px-8 pt-8 w-full ">
+          <img className="rounded-xl w-full" src={ilustratioArticle} alt="Ilustration article" />
+        </div>
+        <button className="m-8 mb-8 p-2 px-4 rounded-sm font-bold text-blog-Gray-950 bg-blog-Yellow">Learning</button>
+        <p className="px-8 font-mono text-blog-Gray-950">
+            Published 21 Dec 2023
+        </p>
+        <h1 className="p-8 font-bold text-xl text-blog-Gray-950">
+          HTML & CSS foundations
+        </h1>
+        <p className="px-8 text-blog-Gray-500">
+          These langueges are the backbone of every website, defining structure, content, and presentation.
+        </p>
+        <div className="flex items-center gap-4 p-8 ">
+          <img src={avatar} alt="Avatar image" />
+          <p className="font-bold">
+            Greeg Hooper
+          </p>
+        </div>
+
+        
+    </div>
+  );
+}
